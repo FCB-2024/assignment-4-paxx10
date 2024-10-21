edbfc1a6ed2a3a5cbf1f64bc190ee4547d289eb1
Pau <paxx10>
2024-10-21 23:03:22 +0200

    enviar

--- src/antiprime.py
+++ src/antiprime.py
@@ -1,17 +1,39 @@
 ## ADD WHATEVER ARGUMENTS ARE NECESSARY TO THE MAIN FUNCTION
 ## IN THE SAME ORDER AS THE ARGUMENTS ARE TAKEN FROM THE
 ## COMMAND LINE SPECIFIED BELOW
+import sys
 def main() :
-	## YOU CODE SHOULD START HERE AST THE SAME
-	## IDENTATION AS THIS COMMENT
+	x = int(sys.argv[1])
+	def comptadorx() :
+		divisorsx = 0
+		i = 1
+		while i <= x :
+			if x % i == 0 :
+				divisorsx = divisorsx + 1
+			i = i + 1
+		return (comptadorx)
+	r = comptadorx
 
-
+	def comptadorpetits() :
+		divisorspetits = 0
+		i2 = 1
+		while i2 < x :
+				if divisorspetits % i2 == 0 :
+					divisorspetits = divisorspetits + 1
+				i2 = i2 + 1
+		return comptadorpetits
+	j = comptadorpetits
+	
+	if j > r :
+		return ('not anti-prime')
+	else :
+		return("anti-prime")
 	## THE LAST LINES OF YOUR CODE SHOULD EITHER
 	## RETURN THE VALUE "anti-prime" or "not anti-prime"
 	## REPLACE THE FOLLOWING LINE BY WHATEVER LINES
 	## OF CODE ALLOW THIS FUNCTION TO RETURN THE VALUE
 	## "anti-prime" or "not anti-prime"
-	return("anti-prime")
+
 
 ## DO NOT REMOVE THIS LINE BELOW
 if __name__ == "__main__" :
